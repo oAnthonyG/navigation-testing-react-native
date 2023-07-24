94dee857af1f0da3b0737b8307e243ca25894017
anthony <anthonytguilherme@hotmail.com>
2023-07-24 18:27:08 -0300

    finish the navigation components

--- App.js
+++ App.js
@@ -4,17 +4,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import AppContacts from "./src/pages/AppContacts";
 import Contacts from "./src/pages/Contacts/index";
 import Information from "./src/pages/Information/index";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="AppContacts" component={AppContacts}></Tab.Screen>
+      <Tab.Screen name="Contacts" component={Contacts}></Tab.Screen>
+    </Tab.Navigator>
+  )
+}
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Contacts" component={Contacts} />
+        <Stack.Screen name="App Contacts" component={Tabs} options={{ headerShown: false }} />
         <Stack.Screen name="Information" component={Information} />
       </Stack.Navigator>
     </NavigationContainer>
